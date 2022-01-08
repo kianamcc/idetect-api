81f2f73d57743d87396f1aecbd7a7526561bfc60
kianamcc <42532725+kianamcc@users.noreply.github.com>
2022-01-07 20:20:38 -0800

    updating  connectionString

--- server.js
+++ server.js
@@ -16,24 +16,15 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const entries = require('./controllers/entries');
 
-app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  // handle OPTIONS method
-  if ('OPTIONS' == req.method) {
-      return res.sendStatus(200);
-  } else {
-      next();
-  }
-});
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 const db = knex({
     client: 'pg',
     connection: {
       connectionString: process.env.DATABASE_URL,
-      ssl: true
+      ssl: {
+        rejectUnauthorized: false
+      }
     }
 });
 
